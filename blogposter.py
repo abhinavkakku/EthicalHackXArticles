a021831a63bc2e03e25f1a5025923f5629f354ba
Abhinav Kumar <abhinav.kakku@gmail.com>
2023-04-05 17:16:03 +0530

    Update blogposter.py

--- blogposter.py
+++ blogposter.py
@@ -19,7 +19,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS articles
 conn.commit()
 
 # Initialize the Telegram bot
-bot_token = 'BOTTOKENHERE'
+bot_token = '6057303793:AAEDBPtVdwdUrVkl68SVPliUyeotosDApn4'
 bot = telegram.Bot(token=bot_token)
 
 # Define the function to scrape the websites and check for new articles
@@ -42,23 +42,7 @@ def scrape_websites():
                 conn.commit()
                 # Send a message to the Telegram bot chat with the new article
                 message = f'New article published:\n\n{title}\n{url}'
-                bot.send_message(chat_id='your_chat_id_here', text=message)
-            
-# Define the function to display the list of articles and ask the user to choose one to post to a channel
-def choose_article(update: Update, context: CallbackContext) -> None:
-    # Get the list of articles from the database
-    c.execute("SELECT * FROM articles")
-    articles = c.fetchall()
-    # Create a list of InlineKeyboardButtons for each article
-    buttons = []
-    for article in articles:
-        title = article[0]
-        url = article[1]
-        buttons.append([InlineKeyboardButton(text=title, callback_data=url)])
-    # Create an InlineKeyboardMarkup with the list of buttons
-    keyboard = InlineKeyboardMarkup(buttons)
-    # Send a message to the user with the list of articles and the keyboard
-    update.message.reply_text('Please choose an article to post to the channel:', reply_markup=keyboard)
+                bot.send_message(chat_id='1578033056', text=message)
 
 # Define the function to handle the callback when the user selects an article to post to a channel
 def post_article(update: Update, context: CallbackContext) -> None:
@@ -79,7 +63,7 @@ def confirm_post(update: Update, context: CallbackContext) -> None:
         query.message.reply_text('Post canceled.')
     else:
         # If the user confirms the post, post the article to the channel
-        chat_id = 'your_channel_id_here'
+        chat_id = '1817346597'
         bot.send_message(chat_id=chat_id, text=data)
         # Send a message to the chat to confirm the post
         query.message.reply_text('Article posted to the channel.')
@@ -88,7 +72,7 @@ def confirm_post(update: Update, context: CallbackContext) -> None:
 
 # Define the function to start the bot and schedule the scraping task to run every hour
 def start_bot():
-    updater = Updater(bot_token)
+    updater = Updater(bot_token, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', choose_article))
     updater.dispatcher.add_handler(CallbackQueryHandler(post_article))
     updater.dispatcher.add_handler(CallbackQueryHandler(confirm_post))
