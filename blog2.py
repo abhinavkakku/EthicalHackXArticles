e712f24525bba873c2be03fc73a6d46ed168c1dc
Abhinav Kumar <abhinav.kakku@gmail.com>
2023-04-05 17:55:44 +0530

    Update blog2.py

--- blog2.py
+++ blog2.py
@@ -1,86 +1,72 @@
-import time
-import logging
 import requests
 from bs4 import BeautifulSoup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+# Enter your bot token here
+bot_token_here = 'YOUR_BOT_TOKEN'
 
-# Global variables
-ARTICLE_LIST = []
+# Enter your chat id here
+your_chat_id_here = 'YOUR_CHAT_ID'
 
-# Function to fetch the latest article titles and URLs
-def fetch_latest_articles():
-    # Define the URLs to check
-    urls = ['https://example.com', 'https://example.org']
+# Enter the channel id you want to post articles to
+your_channel_id_here = 'YOUR_CHANNEL_ID'
 
-    # Iterate through the URLs and fetch the article titles and URLs
-    for url in urls:
+# List of websites to check for new articles
+websites = [
+    {
+        'name': 'Ethical Hack X',
+        'url': 'https://www.ethicalhackx.com/'
+    },
+    {
+        'name': 'Hackaday',
+        'url': 'https://hackaday.com/'
+    },
+    {
+        'name': 'Wired',
+        'url': 'https://www.wired.com/'
+    }
+]
+
+# List of article titles and URLs that have already been posted
+posted_articles = []
+
+
+def check_websites():
+    """Check websites for new articles"""
+    for website in websites:
+        name = website['name']
+        url = website['url']
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         articles = soup.find_all('article')
         for article in articles:
-            title = article.h2.text.strip()
-            url = article.a['href']
-            # Check if the article has already been added to the list
-            if (title, url) not in ARTICLE_LIST:
-                ARTICLE_LIST.append((title, url))
+            title = article.h2.a.text.strip()
+            url = article.h2.a['href']
+            if not any(posted_article['title'] == title for posted_article in posted_articles):
+                posted_articles.append({'title': title, 'url': url})
+                post_to_channel(title, url)
+                send_message(f"New article posted on {name}: {title}\n{url}")
 
-# Function to send the list of latest articles to the user
-def send_article_list(update, context):
-    # Fetch the latest articles
-    fetch_latest_articles()
 
-    # Create the message to send
-    message = "Here are the latest articles:\n\n"
-    for i, (title, url) in enumerate(ARTICLE_LIST):
-        message += f"{i+1}. {title}\n{url}\n\n"
+def send_message(message):
+    """Send message to your chat id"""
+    requests.post(f"https://api.telegram.org/bot{bot_token_here}/sendMessage",
+                  data={"chat_id": your_chat_id_here, "text": message})
 
-    # Send the message to the user
-    update.message.reply_text(message)
 
-    # Reset the article list
-    ARTICLE_LIST.clear()
+def post_to_channel(title, url):
+    """Post the article to the channel"""
+    requests.post(f"https://api.telegram.org/bot{bot_token_here}/sendMessage",
+                  data={"chat_id": your_channel_id_here, "text": f"{title}\n{url}"})
 
-# Function to handle the user's choice of article to post to the channel
-def post_article_choice(update, context):
-    query = update.callback_query
-    query.answer()
-    choice = int(query.data)
 
-    # Post the selected article to the channel
-    title, url = ARTICLE_LIST[choice-1]
-    context.bot.send_message(chat_id='your_channel_id_here', text=f"{title}\n{url}")
-
-    # Reset the article list
-    ARTICLE_LIST.clear()
-
-# Function to start the bot
 def start_bot():
-    # Get the bot token from the environment variable
-    bot_token = 'your_bot_token_here'
-
-    # Create the updater and dispatcher
-    updater = Updater(bot_token)
-    dispatcher = updater.dispatcher
-
-    # Create the command handlers
-    send_article_list_handler = CommandHandler('start', send_article_list)
-    post_article_choice_handler = CallbackQueryHandler(post_article_choice)
-
-    # Add the command handlers to the dispatcher
-    dispatcher.add_handler(send_article_list_handler)
-    dispatcher.add_handler(post_article_choice_handler)
-
-    # Start the bot
+    """Start the Telegram bot"""
+    updater = Updater(bot_token_here)
     updater.start_polling()
-    logger.info('Bot started')
-
-    # Keep the bot running until Ctrl-C is pressed
     updater.idle()
 
+
 if __name__ == '__main__':
+    check_websites()
     start_bot()
